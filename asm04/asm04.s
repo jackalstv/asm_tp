4f2cba8734c4f43c9526cb484fc3385b7722c382
jackalstv <andredor700@gmail.com>
2025-09-30 08:29:04 -0400

    Exo 4 fini

--- asm04/asm04.s
+++ asm04/asm04.s
@@ -5,26 +5,37 @@ section .text
     global _start
 
 _start:
-    mov rax,0
-    mov rdi,0
-    mov rsi, buffer
-    mov rdx, 8
-    syscall
-
-    
-    mov al, byte[buffer]
-    cmp al, '4'
-    jne skip0
-    mov al, byte[buffer+2]
-    cmp al, 0x0A
-    je passkip0
-passkip0:
-
-    mov rax, 60
+    mov rax, 0
     mov rdi, 0
+    mov rsi, buffer
+    mov rdx, 2
+    syscall
+    
+    mov al, [buffer]
+    sub al, '0'         ; Conversion ASCII -> nombre
+    
+    ; Vérifier si c'est un chiffre valide (0-9)
+    cmp al, 9
+    ja Retour2          ; Si > 9, c'est un caractère invalide -> retourne 2
+    
+    ; Vérifier si pair ou impair
+    test al, 1          ; Teste le bit de poids faible
+    jnz Retour1         ; Si bit=1 -> impair -> retourne 1
+    
+    ; Sinon c'est pair -> retourne 0
+    jmp Retour0
+
+Retour0:
+    mov rax, 60
+    mov rdi, 0          ; pair -> 0
     syscall
 
-skip0:  
+Retour1:
     mov rax, 60
-    mov rdi, 1
-    syscall      
+    mov rdi, 1          ; impair -> 1
+    syscall
+
+Retour2:
+    mov rax, 60
+    mov rdi, 2          ; caractère non-chiffre -> 2
+    syscall
