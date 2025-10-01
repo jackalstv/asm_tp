549b414cbe32f54204b6db896c061b06b58289ae
jackalstv <andredor700@gmail.com>
2025-10-01 11:04:07 -0400

    exo7 fini !

--- asm07/asm07.s
+++ asm07/asm07.s
@@ -5,94 +5,71 @@ section .text
     global _start
 
 _start:
-    ; Lecture depuis stdin (2 caractères max: chiffre + newline)
-    mov rax, 0              ; syscall: read
-    mov rdi, 0              ; stdin
-    mov rsi, buffer         ; destination
-    mov rdx, 8              ; nombre d'octets
+    ; Lecture de 2 caractères depuis stdin
+    mov rax, 0
+    mov rdi, 0
+    mov rsi, buffer
+    mov rdx, 2
     syscall
 
+    ; Chargement des deux caractères
+    movzx rax, byte [buffer]      ; Premier chiffre
+    movzx r8, byte [buffer+1]     ; Deuxième chiffre
+    
     ; Conversion ASCII -> nombre
-    movzx rax, byte [buffer]
-    cmp al, '0'
-    jl exit_error
-    cmp al, '9'
-    jg check_two_digits     ; Peut-être un nombre à 2 chiffres
+    sub rax, '0'
+    sub r8, '0'
     
-    sub al, '0'             ; Conversion simple (0-9)
-    jmp test_prime
+    ; Validation des chiffres (0-9)
+    cmp rax, 0
+    jl exitError
+    cmp rax, 9
+    jg exitError
+    cmp r8, 0
+    jl exitError
+    cmp r8, 9
+    jg exitError
+    
+    ; Construction du nombre : n = premier_chiffre * 10 + deuxième_chiffre
+    imul rax, 10
+    add rax, r8
+    mov r10, rax              ; r10 = nombre à tester
+    
+    ; Cas spéciaux
+    cmp r10, 2
+    jl Pasnbpremier           ; 0 et 1 ne sont pas premiers
+    je fin_nmpremier          ; 2 est premier
+    
+    ; Test de divisibilité
+    mov r8, 2                 ; Diviseur initial
 
-check_two_digits:
-    ; Vérifier si c'est un nombre à 2 chiffres
-    movzx rax, byte [buffer]
-    sub al, '0'
-    cmp al, 9
-    ja exit_error           ; Premier caractère invalide
-    cmp al, 0
-    jl exit_error
-    movzx rbx, byte [buffer+1]
-    cmp bl, 10              ; newline?
-    je test_prime           ; Non, c'était un seul chiffre
-    sub bl, '0'
-    imul ax, 10
-    add al, bl
+loop_nmpremier:
+    mov rax, r10
+    xor rdx, rdx              ; Mettre rdx à 0 avant division
+    div r8                    ; rax = quotient, rdx = reste
     
-test_prime:
-    ; RAX contient maintenant le nombre à tester
-    mov r8, rax             ; Sauvegarder le nombre original dans r8
+    cmp rdx, 0                ; Si reste == 0, le nombre n'est pas premier
+    je Pasnbpremier
     
-    ; Cas spéciaux: 0, 1 ne sont pas premiers
-    cmp r8, 2
-    jl not_prime
+    inc r8                    ; Diviseur suivant
+    mov rax, r8
+    imul rax, rax             ; rax = r8²
+    cmp rax, r10              ; Si r8² > n, n est premier
+    jg fin_nmpremier
     
-    ; 2 est premier
-    cmp r8, 2
-    je is_prime
+    jmp loop_nmpremier
 
-    cmp r8,1
-    je not_prime
-    
-    ; Les nombres pairs ne sont pas premiers
-    test r8b, 1
-    jz not_prime
-    
-    ; Tester les diviseurs de 3 à sqrt(n)
-    mov rbx, 3              ; Diviseur courant
-    
-loop_divisors:
-    mov rax, rbx
-    mul rbx             ; RAX = RBX²
-    cmp rax, r8
-    jg is_prime
-    
-    ; Test de divisibilité: nombre % diviseur == 0 ?
-    mov rax, r8             ; Nombre à tester
-    xor rdx, rdx            ; Clear RDX pour la division
-    div rbx                 ; RAX = quotient, RDX = reste
-    
-    cmp rdx, 0              ; Reste == 0 ?
-    je not_prime            ; Oui -> pas premier
-    cmp rax,1
-    je not_prime 
-    
-    ; Passer au diviseur suivant (seulement impairs)
-    add rbx, 2
-    jmp loop_divisors
-
-is_prime:
-    ; Code de sortie 0 = premier
-    mov rax, 60             ; syscall: exit
-    mov rdi, 0              ; code de sortie: 0 (premier)
-    syscall
-
-not_prime:
-    ; Code de sortie 1 = pas premier
-    mov rax, 60             ; syscall: exit
-    mov rdi, 1              ; code de sortie: 1 (pas premier)
-    syscall
-
-exit_error:
-    ; Code de sortie 2 = erreur d'entrée
+Pasnbpremier:
     mov rax, 60
-    mov rdi, 2
+    mov rdi, 1                ; Pas premier -> code 1
+    syscall
+
+exitError:
+    mov rax, 60
+    mov rdi, 2                ; Erreur -> code 2
+    syscall
+
+fin_nmpremier:
+    mov rax, 60
+    mov rdi, 0                ; Premier -> code 0
     syscall
